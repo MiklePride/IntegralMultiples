de8b23ca1eabaa4fe55911bcffacb05bcada703b
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-23 19:21:04 +0300

    Добавьте файлы проекта.

---
+++ IntegralMultiples/Program.cs
@@ -0,0 +1,25 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int minimumRandomNumber = 1;
+        int maximumRandomNumber = 28;
+        int maximumMultipleNumber = 1000;
+        int minimumMultipleNumber = 100;
+        Random random = new Random();
+        int randomNumber = random.Next(minimumRandomNumber, maximumRandomNumber);
+
+        for(int i = randomNumber; i < maximumMultipleNumber; i += randomNumber)
+        {
+            if(i < minimumMultipleNumber)
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
